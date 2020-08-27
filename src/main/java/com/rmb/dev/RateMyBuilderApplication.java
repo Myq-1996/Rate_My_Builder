7d08052f448073ed772a0a9c43210149a168bd83
Yanqing Mao <u6924029@anu.edu.au>
2020-08-28 01:14:13 +0800

    Initial commit

---
+++ src/main/java/com/rmb/dev/RateMyBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.rmb.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateMyBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateMyBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rmb/dev/RateMyBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmb.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateMyBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
